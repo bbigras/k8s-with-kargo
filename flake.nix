ddc48fc2dc877f2d63a7ff75b52a9b7419ca436d
Bruno Bigras <bigras.bruno@gmail.com>
2025-02-26 14:19:35 -0500

    nix shell with kubeseal

--- flake.nix
+++ flake.nix
@@ -15,14 +15,19 @@
         # 3. Add here: foo.flakeModule
 
       ];
-      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      systems = [ "x86_64-linux" ];
       perSystem = { config, self', inputs', pkgs, system, ... }: {
         # Per-system attributes can be defined here. The self' and inputs'
         # module parameters provide easy access to attributes of the same
         # system.
 
-        # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
-        packages.default = pkgs.hello;
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            kubeseal
+            nil
+            nixd
+          ];
+        };
       };
       flake = {
         # The usual flake attributes can be defined here, including system-
